67d8e01c6004937c7c0e06908068fbab02ddfc63
Anton Lyxell <git@lyxell.dev>
2024-12-07 19:03:38 +0100

    clean up dec 02

--- 02_01.sql
+++ 02_01.sql
@@ -1,34 +1,45 @@
-create table T (c1 text) strict;
+create table T (row text);
 .import 02_input.txt T
 .load ./regex0.so
 
 with
 
--- parse input
-L(row, col, curr, prev) as (
+-- P contains the problem input
+--
+-- r is the row index
+-- c is the column index
+-- v is the value at (r, c)
+P(r, c, v) as (
 	select
 		T.rowid,
 		M.rowid,
-		cast(M.match as integer),
-		-- lag value by 1
-		lag(cast(M.match as integer)) over (partition by T.rowid)
+		cast(M.match as integer)
 	from
 		T,
-		regex_find_all("\d+", T.c1) as M
+		regex_find_all("\d+", T.row) as M
+),
+
+-- L contains all adjacent pairs
+-- of values for each row
+--
+-- r is the row index
+-- pred is the first value of the pair
+-- succ is the second value of the pair
+L(r, pred, succ) as (
+	select
+		r,
+		v,
+		lag(v) over (partition by r order by c)
+	from P
 )
 
--- group by row and find all rows without rule violations
-select count(*) from (
-	select
-		L.row,
-		-- we construct these values to be able to do boolean aggregation
-		case when (abs(prev - curr) > 3) then 1 else 0 end as diff,
-		case when (prev <= curr) then 1 else 0 end as incr,
-		case when (prev >= curr) then 1 else 0 end as decr
-	from L
-	group by L.row
-	having
-		sum(diff) == 0 and
-		(sum(incr) == 0 or sum(decr) == 0)
+select count(*)
+from (
+    select r
+    from L
+    group by r
+    having
+        sum(abs(pred - succ) > 3) = 0
+        and (sum(pred <= succ) = 0 or
+			 sum(pred >= succ) = 0)
 );
-
--- 02_02.sql
+++ 02_02.sql
@@ -1,39 +1,58 @@
-create table T (c1 text) strict;
+create table T (row text);
 .import 02_input.txt T
 .load ./regex0.so
 
 with
 
--- parse input
-L(row, discard, col, curr, prev) as (
+-- P contains the problem input
+--
+-- r is the row index
+-- c is the column index
+-- v is the value at (r, c)
+P(r, c, v) as (
 	select
 		T.rowid,
-		D.value,
 		M.rowid,
-		cast(M.match as integer),
-		-- lag value by 1
-		lag(cast(M.match as integer)) over (partition by T.rowid, D.value)
+		cast(M.match as integer)
 	from
 		T,
-		regex_find_all("\d+", T.c1) as M
-	-- generate index of columns to discard
-	join generate_series(0, 7) as D
-	where D.value != M.rowid
+		regex_find_all("\d+", T.row) as M
+),
+
+-- D contains all the columns indices
+-- that we will discard
+--
+-- dr is the row index
+-- dc is the column index to discard
+D(dr, dc) as (
+	select r, c from P
+),
+
+-- L contains all adjacent pairs
+-- of values partitioned by the row index
+-- and the index of the discarded column
+--
+-- r is the row index
+-- dc is the discarded column
+-- pred is the first value of the pair
+-- succ is the second value of the pair
+L(r, dc, pred, succ) as (
+	select
+		r,
+		dc,
+		v,
+		lag(v) over (partition by r, dc order by c)
+	from P
+	join D on r = dr
+	where c != dc
 )
 
--- group by row and find all rows without rule violations
-select count(distinct row) from (
-	select
-		L.row,
-		-- we construct these values to be able to do boolean aggregation
-		-- this let's us express statements that should hold true for all
-		-- elements
-		case when (abs(prev - curr) > 3) then 1 else 0 end as diff,
-		case when (prev <= curr) then 1 else 0 end as incr,
-		case when (prev >= curr) then 1 else 0 end as decr
-	from L
-	group by L.row, L.discard
-	having
-		sum(diff) == 0 and
-		(sum(incr) == 0 or sum(decr) == 0)
+select count(distinct r)
+from (
+    select r
+    from L
+    group by r, dc
+    having
+        sum(abs(pred - succ) > 3) = 0
+        and (sum(pred <= succ) = 0 or sum(pred >= succ) = 0)
 );
