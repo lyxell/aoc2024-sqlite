37dbad180f613d82e03e3761e03d342459da6f58
Anton Lyxell <git@lyxell.dev>
2024-12-08 13:54:55 +0100

    dec 08

---
+++ 08_02.sql
@@ -0,0 +1,35 @@
+create table T (c1 text) strict;
+.import 08_input.txt T
+.load ./regex0.so
+.mode box
+
+-- parse input
+with
+
+-- M is the parsed map
+--
+-- x is the column index
+-- y is the row index
+-- c is the char at (x, y)
+M(x, y, c) as materialized (
+	select
+		R.start,
+		T.rowid - 1,
+		R.match
+	from regex_find_all(".", T.c1) as R
+	join T
+)
+
+select count(*) from (
+	select distinct
+		(M1.x + (M1.x - M2.x) * X.value) as px,
+		(M1.y + (M1.y - M2.y) * X.value) as py
+	from M as M1
+	join M as M2 on
+		M1.c = M2.c and
+		M1.c != '.' and
+		(M1.x != M2.x or M1.y != M2.y)
+	join generate_series(0, (select max(max(x),max(y)) from M)) as X
+	where
+		(px, py) in (select x, y from M)
+)
